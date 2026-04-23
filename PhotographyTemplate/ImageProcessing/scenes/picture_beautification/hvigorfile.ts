194a576c7d78afcdefacb85d84d1691146723340
唐笑阳 <23371531>
2026-04-23 20:50:31 +0800

    feat: 添加 StickerToolBar 测试用例

--- PhotographyTemplate/ImageProcessing/scenes/picture_beautification/hvigorfile.ts
+++ PhotographyTemplate/ImageProcessing/scenes/picture_beautification/hvigorfile.ts
@@ -1,6 +1,54 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
+
+const syncOhosTestHostPagePlugin = {
+  pluginId: 'syncOhosTestHostPagePlugin',
+  apply(node) {
+    node.afterNodeEvaluate(() => {
+      const compileTask = node.getTaskByName('ohosTest@OhosTestCompileArkTS')
+        ?? node.getTaskByName('OhosTestCompileArkTS');
+
+      if (!compileTask) {
+        return;
+      }
+
+      compileTask.beforeRun(() => {
+        const sourcePath = path.resolve(__dirname, 'src/ohosTest/ets/testability/pages/Index.ets');
+        const targetPath = path.resolve(__dirname, '.test/default/intermediates/src/ohosTest/ets/testability/pages/Index.ets');
+        if (!fs.existsSync(sourcePath)) {
+          return;
+        }
+
+        const targetDir = path.dirname(targetPath);
+        const replacements = new Map([
+          ['../../../../main/ets/model/BeautificationParam',
+            path.relative(targetDir, path.resolve(__dirname, 'src/main/ets/model/BeautificationParam.ets'))
+              .replace(/\\/g, '/')
+              .replace(/\.ets$/, '')],
+          ['../../../../main/ets/components/StickerToolBar',
+            path.relative(targetDir, path.resolve(__dirname, 'src/main/ets/components/StickerToolBar.ets'))
+              .replace(/\\/g, '/')
+              .replace(/\.ets$/, '')],
+          ['../../../../main/ets/model/Constants',
+            path.relative(targetDir, path.resolve(__dirname, 'src/main/ets/model/Constants.ets'))
+              .replace(/\\/g, '/')
+              .replace(/\.ets$/, '')]
+        ]);
+
+        let sourceContent = fs.readFileSync(sourcePath, 'utf-8');
+        replacements.forEach((targetImportPath, sourceImportPath) => {
+          sourceContent = sourceContent.replace(sourceImportPath, targetImportPath);
+        });
+
+        fs.mkdirSync(targetDir, { recursive: true });
+        fs.writeFileSync(targetPath, sourceContent);
+      });
+    });
+  }
+};
 
 export default {
   system: harTasks,
-  plugins: []
+  plugins: [syncOhosTestHostPagePlugin]
 }
