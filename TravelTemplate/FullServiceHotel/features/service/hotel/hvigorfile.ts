bbcaa60ed5152c330a425e614fa74371bf24e2cf
VoidInHeart <2707168661@qq.com>
2026-07-22 16:09:57 +0800

    feat: 为HotelOrderPage.ets添加测试用例

--- TravelTemplate/FullServiceHotel/features/service/hotel/hvigorfile.ts
+++ TravelTemplate/FullServiceHotel/features/service/hotel/hvigorfile.ts
@@ -1,6 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import { hvigor } from '@ohos/hvigor';
 import { hspTasks } from '@ohos/hvigor-ohos-plugin';
+import type { HvigorPlugin } from '@ohos/hvigor';
+
+const plugin: HvigorPlugin = {
+  pluginId: 'hotel_replace_ohos_test_index',
+  apply(node) {
+    hvigor.nodesEvaluated(() => {
+      if (!(hvigor.getCommandEntryTask() ?? []).includes('onDeviceTest')) {
+        return;
+      }
+      node.registerTask({
+        name: 'ReplaceOhosTestIndex',
+        dependencies: ['ohosTest@GenerateOhosTestTemplate'],
+        postDependencies: ['ohosTest@OhosTestCompileArkTS'],
+        run(taskContext) {
+          const sourcePath = path.resolve(taskContext.modulePath, 'src/ohosTest/ets/testability/pages/Index.ets');
+          const targetPath = path.resolve(taskContext.modulePath,
+            'build/default/intermediates/src/ohosTest/ets/testability/pages/Index.ets');
+          if (fs.existsSync(sourcePath)) {
+            fs.mkdirSync(path.dirname(targetPath), { recursive: true });
+            fs.copyFileSync(sourcePath, targetPath);
+          }
+        }
+      });
+    });
+  }
+};
 
 export default {
-  system: hspTasks /* Built-in plugin of Hvigor. It cannot be modified. */,
-  plugins: [] /* Custom plugin to extend the functionality of Hvigor. */,
+  system: hspTasks,
+  plugins: [plugin],
 };
